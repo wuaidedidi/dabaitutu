663064ac61b96b6501743fad7ff98fb33f7f31ef
wuaidedidi <470357714@qq.com>
2022-08-19 13:05:14 +0800

    this is first commit

---
+++ src/main/java/com/lichen/dabaitutu/DabaitutuApplication.java
@@ -0,0 +1,13 @@
+package com.lichen.dabaitutu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabaitutuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabaitutuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lichen/dabaitutu/DabaitutuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lichen.dabaitutu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabaitutuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
