ec9b7b91a08ae66d76e84c652b62b5fb5b18ab93
wuaidedidi <470357714@qq.com>
2022-08-19 13:49:07 +0800

    编写登陆页面

---
+++ src/main/java/com/lichen/dabaitutu/system/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.lichen.dabaitutu.system.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lichen
+ * @version 1.0.0
+ * @className LoginController.java
+ * @Description TODO
+ * @createTime 2022年08月19日 13:44:00
+ */
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+        public void login(){
+
+        }
+}
